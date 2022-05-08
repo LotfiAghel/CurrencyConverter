8d7571a651f0f1ab54f385c5f638b96aee4d004b
f <fatholah@gmail.com>
2022-05-08 15:14:25 +0430

    pass first Test

--- FloydWarshallCurrencyConverter.cs
+++ FloydWarshallCurrencyConverter.cs
@@ -18,13 +18,15 @@ class FloydWarshallCurrencyConverter : ICurrencyConverter
     /// <summary>
     ///    in this array, we save the chosen shortest path length between i, j
     /// </summary>
-    int[,] ShortPathMatrix ;
+    int[,] ShortPathLength ;
 
     /// <summary>
     ///    in this array, we save one of the vertexes the chosen shortest path between i, j. 
-    ///    if  ShortPathFinder[i][j]==k  thats mean (i)--->(k)--->(j) is one of shortess math bitwen i and j 
+    ///    if  ShortPathFinder[i,j]==k  thats mean (i)--->(k)--->(j) is one of shortess math bitwen i and j 
     /// </summary>
     int[,] ShortPathFinder ;
+
+    double[,] ShortPathValue ;
 
 
     public void ClearConfiguration(){
@@ -36,31 +38,56 @@ class FloydWarshallCurrencyConverter : ICurrencyConverter
                 CurrencyNameToIndex[a]=CurrencyNameToIndex.Count;
             }
     }
+
+    private void doFloyd(){
+        for(int i=0; i<n; ++i)
+            for(int j=0; j<n; ++j)
+                for(int k=0; k<n; ++k){
+                    if(ShortPathLength[i,j]>ShortPathLength[i,k]+ShortPathLength[k,j]){
+
+                        ShortPathLength[i,j]=ShortPathLength[i,k]+ShortPathLength[k,j];
+                        ShortPathValue[i,j]=ShortPathValue[i,k]*ShortPathValue[k,j];
+                        ShortPathFinder[i,j]=k;
+
+                    }                    
+                }
+    }
+
     public void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates){
         // TODO set write lock
         configurations.Clear();
         
-        foreach(var i in configurations){
+        foreach(var i in conversionRates){
             this.configurations.Add(i);
             indexNewCurrency(i.Item1);
             indexNewCurrency(i.Item2);
         }
         
         n=CurrencyNameToIndex.Count;
-        ShortPathMatrix=new int[n,n];
-        ShortPathFinder=new int[n,n];
+        ShortPathLength = new int[n,n];
+        ShortPathFinder = new int[n,n];
+        ShortPathValue  = new double[n,n];
         for(int i=0; i<n ; ++i)
             for(int j=0; j<n ; ++j){
-              ShortPathMatrix[i,j]=1000000;
+              ShortPathLength[i,j]=1000000;
               ShortPathFinder[i,j]=-1;
+              ShortPathValue[i,j]=double.NaN;
             }
-       
+        foreach(var i in configurations){
+            ShortPathLength[CurrencyNameToIndex[i.Item1],CurrencyNameToIndex[i.Item2]]=1;
+            ShortPathLength[CurrencyNameToIndex[i.Item2],CurrencyNameToIndex[i.Item1]]=1;
+
+            ShortPathValue[CurrencyNameToIndex[i.Item1],CurrencyNameToIndex[i.Item2]]=i.Item3;
+            ShortPathValue[CurrencyNameToIndex[i.Item2],CurrencyNameToIndex[i.Item1]]=1.0/i.Item3;
+        }
+        doFloyd();
     }
 
 
 
     public double Convert(string fromCurrency, string toCurrency, double amount){
         // TODO set read lock
-        return 0;
+        return  ShortPathValue[CurrencyNameToIndex[fromCurrency],CurrencyNameToIndex[toCurrency]]*amount;
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var test=new Test1();
+test.run();
